919a737059c49a4becffe7763a0dd2747dfb82b3
KO1231 <k.oka@kazutech.jp>
2025-01-08 20:49:42 +0900

    add: 05

--- src/main/java/com/example/station5/Main.java
+++ src/main/java/com/example/station5/Main.java
@@ -3,6 +3,14 @@ package com.example.station5;
 public class Main {
     public static void main(String[] args) {
         // ここから
+        int
+         userA = 23,
+         userB = 36,
+         userC = 31,
+         userD = 48,
+         userE = 58;
+
+        System.out.println((double) (userA+userB+userC+userD+userE) / 5);
         // ここまで
     }
 }
