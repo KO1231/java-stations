fa2bb4dbbbf0533dc8a92182824cd2a529583b15
KO1231 <k.oka@kazutech.jp>
2025-01-08 21:02:23 +0900

    add: 09

--- src/main/java/com/example/station9/Main.java
+++ src/main/java/com/example/station9/Main.java
@@ -1,5 +1,7 @@
 package com.example.station9;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         int[] itemPrices1 = {1000, 1100, 1200, 1300};
@@ -15,6 +17,28 @@ public class Main {
 
     public static void test(int[] itemPrices) {
         // ここから
+
+        int resultA = Arrays.stream(itemPrices).sum();
+        // 送料無料
+
+        int resultB = Arrays.stream(itemPrices)
+                        .map(price -> (int) (price * 0.9)) // 10% off
+                        .sum();
+        resultB += resultB >= 5000 ? 0 : 800; // 送料
+
+        int resultC = Arrays.stream(itemPrices)
+                        .map(price -> price >= 2000 ? (int) (price * 0.8) : price) // 2000円以上に対して 20% off
+                        .sum();
+        resultC += resultC >= 5000 ? 0 : 800; // 送料
+
+        if(resultA < resultB && resultA < resultC) {
+            System.out.println("A");
+        } else if(resultB < resultA && resultB < resultC) {
+            System.out.println("B");
+        } else {
+            System.out.println("C");
+        }
+        
         // ここまで
     }
 }
