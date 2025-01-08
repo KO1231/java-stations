c7063d02bae6b70120dedb49436001075181768a
KO1231 <k.oka@kazutech.jp>
2025-01-08 20:51:23 +0900

    add: 06

--- src/main/java/com/example/station6/Main.java
+++ src/main/java/com/example/station6/Main.java
@@ -1,8 +1,14 @@
 package com.example.station6;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         // ここから
+        int[] users = {23,36,31,48,58};
+
+        int all = Arrays.stream(users).sum();
+        System.out.println((double) all / 5);
         // ここまで
     }
 }
